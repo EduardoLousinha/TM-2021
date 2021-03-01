6b962078ed7a7c9de94fd5fd1f47250bf870696a
Eduardo Lousinha <edulousinha@gmail.com>
2021-03-01 17:46:26 +0000

    Exercício 1

---
+++ Aula 2/exercicio1/EduardoLousinha-25871/script.js
@@ -0,0 +1,8 @@
+function readInputFromText() {
+    const textFromInput = document.getElementById("inputText").value
+    if (textFromInput) {
+        document.getElementById("viewerText").innerText = textFromInput
+    } else {
+        alert("Escreva algo no Input")
+    }
+}
